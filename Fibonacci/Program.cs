b0fa07d8f55f1572838e4d2378e4bb26ac2bcd6f
The Very Cool School Pc <ddd.com>
2019-03-29 12:56:33 +0000

    added recursive in fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static int Fibonacci(int n)
+        {
+            int res;
+            if (n <= 2)
+            {
+                res = 1;
+            }
+            else
+            {
+                res = Fibonacci(n-1) + Fibonacci(n-2);
+            }
+            return res;
+        }
+        
+
+        static void Main(string[] args)
+        {
+            string numeroS;
+            int n;
+
+            Console.WriteLine("Insira um número inteiro.");
+            numeroS = Console.ReadLine();
+            
+            n = Convert.ToInt32(numeroS);
+
+            Console.WriteLine("O número na posição pedida é o {0}", Fibonacci(n));
+        }
+    }
+}
