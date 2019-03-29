772061cad723879012bc4ed0b2638ab042e2cb46
The Very Cool School Pc <ddd.com>
2019-03-29 11:16:03 +0000

    added xml comments in order to aid in code reading

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -2,19 +2,33 @@
 
 namespace MyFirstMethod
 {
+    /// <summary>
+    /// class program that has the to be executed code
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// a method that counts all the way to a number n and prints it
+        /// </summary>
+        /// <param name="n">an int that the method will count up to</param>
         static void CountToN(int n)
         {
             for (int i = 1; i <= n; i++)
                 Console.WriteLine(i);
         }
 
+        /// <summary>
+        /// a method that will use the previous method, CountToN, to count to 10
+        /// </summary>
         static void CountTo10()
         {
             CountToN(10);
         }
 
+        /// <summary>
+        /// the method that calls the other methods in order do display the results
+        /// </summary>
+        /// <param name="args">used to give the methods the n value required</param>
         static void Main(string[] args)
         {
 
